806ca5048add4a546995398780449a914c382ba5
Markus Roellig <roellig@ph1.uni-koeln.de>
2021-02-23 23:35:09 +0100

    V1.0

---
+++ PacletInfo.m
@@ -0,0 +1,11 @@
+(* Paclet Info File *)
+
+(* created 2017.03.06*)
+
+Paclet[
+  Name -> "LTE",
+  Version -> "0.0.1",
+  MathematicaVersion -> "6+",
+  Extensions -> {
+    {"Documentation", Language -> "English"}
+}]
